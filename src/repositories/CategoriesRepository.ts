18d7d1d651ba125f4d291e7c148eaa828a8e110f
Davi <davi.freitas@useall.com.br>
2022-02-08 10:50:41 -0300

    Princípio da Substituição Liskov

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,39 +1,39 @@
-import { Category } from "../model/Category"
+import { Category } from "../model/Category";
+import {
+    ICategoriesRepository,
+    ICreateCategoryDTO,
+} from "./ICategoriesRepository";
 
-// DTO Data transfer Object
-interface ICreateCategoryDTO {
-    name: string; 
-    description: string;
-}
-
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
     private categories: Category[];
 
     constructor() {
         this.categories = [];
-    };
+    }
 
     create({ name, description }: ICreateCategoryDTO): void {
         const category = new Category();
-        
-        Object.assign(category, { 
-            name, 
-            description, 
-            created_at: new Date() 
+
+        Object.assign(category, {
+            name,
+            description,
+            created_at: new Date(),
         });
 
         this.categories.push(category);
-    };
+    }
 
     list(): Category[] {
         return this.categories;
-    };
+    }
 
-    findByName (name: string): Category {
-        const category = this.categories.find(category => category.name === name);
+    findByName(name: string): Category {
+        const category = this.categories.find(
+            (category) => category.name === name
+        );
 
         return category;
     }
 }
 
-export { CategoriesRepository }
+export { CategoriesRepository };
