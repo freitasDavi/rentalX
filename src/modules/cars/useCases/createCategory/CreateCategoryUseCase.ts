7c001d1780328cfd5f357b5dd2c73548938fce85
Davi <davi.freitas@useall.com.br>
2022-02-08 11:49:31 -0300

    useCases CreateCategory

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,17 +1,11 @@
-import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
     name: string;
     description: string;
 }
 
-/**
- * [x] - Definir o tipo de retorno
- * [x] - Alterar o retorno de erro
- * [x] - Acessar o repositório
- */
-
-class CreateCategoryService {
+class CreateCategoryUseCase {
     constructor(private categoriesRepository: ICategoriesRepository) {}
 
     execute({ name, description }: IRequest): void {
@@ -26,4 +20,4 @@ class CreateCategoryService {
     }
 }
 
-export { CreateCategoryService };
+export { CreateCategoryUseCase };
