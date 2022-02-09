da8b6844554056aec4ec528e59e196163de1b2a0
UbDavi <davi.frrs@outlook.com>
2022-02-09 09:01:16 -0300

    Category refactoring

---
+++ readme.md
@@ -0,0 +1,6 @@
+# yarn typeorm migration:create -n CreateCategories
+## Cria a migration para de acordo com o nome selecionado
+# yarn typeorm migation:run
+## Roda a migration no banco
+# yarn typeorm migration:revert 
+## Reverte a criação da migration
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
-import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import createCategoryController from "../modules/cars/useCases/createCategory";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
@@ -12,7 +12,7 @@ const upload = multer({
 
 categoriesRoutes.post("/", (request, response) => {
     console.log("reload funcionando");
-    return createCategoryController.handle(request, response);
+    return createCategoryController().handle(request, response);
 });
 
 categoriesRoutes.get("/", (request, response) => {
