d94a848cd627495d9ba1a2a3be68ab4d946964fc
UbDavi <davi.frrs@outlook.com>
2022-02-09 16:24:45 -0300

    k

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcryptjs";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository"
 
@@ -10,8 +11,17 @@ class CreateUserUseCase {
     ) {}
 
     async execute({ driver_license, email, name, password }: ICreateUserDTO): Promise<void> {
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+        if(userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+        
+        const hashPassword = await hash(password.toString(), 8);
+        
+
         await this.usersRepository.create({
-            name, driver_license, email, password
+            name, driver_license, email, password: hashPassword
         });
     }
 }
